410296255acb5167914b0b00925253b70d5244c5
DesmondNoutcha <alouis@gmail.com>
2016-06-21 17:10:40 +0200

    Franck

--- local/resources/views/welcome.blade.php
+++ local/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Danick Dayola</div>
+                <div class="title">Franck Stiffler </div>
             </div>
         </div>
     </body>
